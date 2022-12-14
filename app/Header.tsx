684f4a2558242068ffb625b29e7d98a147865a28
JAMIUL ISLAM <tomcat1112k@JAMIULs-MacBook-Pro.local>
2022-12-14 23:01:56 +0600

    initializing project

---
+++ app/Header.tsx
@@ -0,0 +1,13 @@
+import { Bars3Icon } from "@heroicons/react/24/solid";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <Bars3Icon className="h-8 w-8 cursor-pointer" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
